8b7dda2b33d6a57808167197a97753f7042aad66
Carlos Eduardo <carloseduardorei@gmail.com>
2026-08-19 13:06:04 -0300

    Implement prime number checker in exp2.java

---
+++ JAVA/Aula 3/exercisios/Numero Primo/exp2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner dado = new Scanner(System.in);
+        int n = dado.nextInt();
+        int cont;
+        for (int i = 0; i < n; i++) {
+            int num = dado.nextInt();
+            cont = 0;
+            if (num == 2) {
+                System.out.println(num + " eh primo");
+            } else if (num==0||num==1) {
+                System.out.println(num+" nao eh primo");
+            } else {
+                for (int j = 2; j <= Math.sqrt(num); j++) {
+                    if (num%j == 0) {
+                        cont++;
+                        break;
+                    }
+                }
+                if (cont>0){
+                    System.out.println(num+" nao eh primo");
+                }else{
+                    System.out.println(num + " eh primo");
+                }
+            }
+        }
+    }
+}
