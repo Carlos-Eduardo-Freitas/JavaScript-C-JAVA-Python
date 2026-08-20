d6c7d8ba1d488e419fc5786ce4071eb925028f70
Carlos Eduardo <carloseduardorei@gmail.com>
2026-08-20 12:33:56 -0300

    Update Lanche.java

--- JAVA/Aula 5/exercícios/Lanche.java
+++ JAVA/Aula 5/exercícios/Lanche.java
@@ -4,10 +4,10 @@ public class Main {
         Scanner dado = new Scanner(System.in);
         int cod = dado.nextInt();
         int n = dado.nextInt();
-        double[] precos = {4.00,4.50,5.00,2.00,1.50};
+        double[] precos = {0,4.00,4.50,5.00,2.00,1.50};
         double total;
             if (cod >= 1 && cod <= 5) {
-                total = precos[cod-1] * n;
+                total = precos[cod] * n;
             } else {
                 total = 0;
             }
