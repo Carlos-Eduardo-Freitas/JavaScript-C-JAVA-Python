1720744d155e404d4d7f59ed90452b89738e94dd
Carlos Eduardo <carloseduardorei@gmail.com>
2026-08-29 16:43:32 -0300

    Implement animal classification based on characteristics

---
+++ JAVA/Aula 2/exercicios-condicionais/Animal.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        if (!sc.hasNext()) return;
+        String p1 = sc.next();
+        String p2 = sc.next();
+        String p3 = sc.next();
+
+        if (p1.equals("vertebrado")) {
+            if (p2.equals("ave")) {
+                if (p3.equals("carnivoro")) {
+                    System.out.println("aguia");
+                } else if (p3.equals("onivoro")) {
+                    System.out.println("pomba");
+                }
+            } else if (p2.equals("mamifero")) {
+                if (p3.equals("onivoro")) {
+                    System.out.println("homem");
+                } else if (p3.equals("herbivoro")) {
+                    System.out.println("vaca");
+                }
+            }
+        } else if (p1.equals("invertebrado")) {
+            if (p2.equals("inseto")) {
+                if (p3.equals("hematofago")) {
+                    System.out.println("pulga");
+                } else if (p3.equals("herbivoro")) {
+                    System.out.println("lagarta");
+                }
+            } else if (p2.equals("anelideo")) {
+                if (p3.equals("hematofago")) {
+                    System.out.println("sanguessuga");
+                } else if (p3.equals("onivoro")) {
+                    System.out.println("minhoca");
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
